b38d8517c89c0d63503a5b83790074df8bb5c88e
dello-dev <dello.dev@gmail.com>
2023-06-18 20:43:02 -0700

    done with billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { apple, bill, google } from "../asserts";
+import styles, {layout} from "../style"
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
+const Billing = () =>(
+    <section id='product' className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
+        <div className='absolute z-[3] left-1/2 top-0 w-[50%] h-[50%] white__gradient rounded-full'/>
+        <div className='absolute z-[3] left-1/2 bottom-0 w-[50%] h-[50%] pink__gradient rounded-full'/>
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/> billing & invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi illo laboriosam non autem, dolorum corporis delectus exercitationem asperiores veritatis totam perspiciatis qui quisquam.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+          <img src={google} alt="google_play" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+        </div>
+      </div>
+
+    </section>
   )
-}
 
 export default Billing
