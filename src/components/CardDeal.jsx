b73b9394351ae6d5ace7d83ae3873d16f09dce55
dello-dev <dello.dev@gmail.com>
2023-06-18 20:53:27 -0700

    done with the card deal seaction

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { card } from "../asserts";
+import styles, {layout} from "../style"
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
+const CardDeal = () =>(
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" />
+        in few easy steps</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit voluptates vero, architecto reprehenderit neque repellendus qui quam nulla recusandae sequi at sit.</p>
+        <Button styles="mt-10"></Button>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]'/>
+      </div>
+    </section>
   )
-}
 
 export default CardDeal
